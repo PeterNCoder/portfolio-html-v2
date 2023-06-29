5b7b89bf61ff1408b3547acf35ce4de1b9ab7216
Peter Nguyen <peternguyenwebdev@gmail.com>
2023-06-28 22:20:20 -0700

    Typography change and remove leading 0s from clock

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,6 @@ let time = document.getElementById("current-time");
 
 (function displayTime() {
     let date = new Date();
-    time.textContent = date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" }).replaceAll('.', '');
+    time.textContent = date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" }).replaceAll('.', '').replace(/^0+/, '');
     setTimeout(displayTime, 1000);
 })();
