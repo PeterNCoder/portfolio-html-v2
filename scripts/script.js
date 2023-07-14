239e291efb5a34450eb7d8738caa9f6aeaabac82
Peter Nguyen <peternguyenwebdev@gmail.com>
2023-07-13 22:58:27 -0700

    works.html animations

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,20 @@ let time = document.getElementById("current-time");
     time.textContent = date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" }).replaceAll('.', '').replace(/^0+/, '');
     setTimeout(displayTime, 1000);
 })();
+
+const animations = {
+    boxes: document.querySelectorAll('.work-container'),
+    triggerBottom: window.innerHeight / 5 * 4,
+
+    checkBoxes: () => {
+        window.addEventListener('scroll', animations.checkBoxes);
+        animations.boxes.forEach(box => {
+            if(box.getBoundingClientRect().top < animations.triggerBottom) {
+                box.classList.add('show');
+            } else {
+                box.classList.remove('show');
+            }
+        })
+    }
+}
+animations.checkBoxes();
